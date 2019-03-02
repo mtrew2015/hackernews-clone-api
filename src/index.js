73911f2df6a8071b676bd712b0d830ea29170289
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2019-03-02 14:52:23 -0500

    added a few resolvers

--- src/index.js
+++ src/index.js
@@ -4,13 +4,31 @@ const {GraphQLServer} = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed:[Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 //2 
+let links = [{
+  id: 'link-0',
+  url: 'www.lambdaschool.com', 
+  description: 'the most awesome school there is'
+}]
 
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url:(parent) => parent.url,
   }
 }
 
